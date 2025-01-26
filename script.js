38cf08e7f3ff157900dd7079cd78332f04203e46
kanishk tiwari <kanishk.swe@gmail.com>
2025-01-26 17:17:25 +0530

    creating our own combineReducers

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-import { combineReducers, createStore } from "redux";
+import { createStore } from "redux";
 import cartReducer, { CART_ADD_ITEM, CART_ITEM_DECREASE_QUANTITY, CART_ITEM_INCREASE_QUANTITY } from "./cartReducer";
 import wishlistReducer, { WISHLIST_ADD_ITEM, WISHLIST_REMOVE_ITEM } from "./wishlistReducer";
 import productsReducer from "./productsReducer";
+
+function combineReducers(reducers) {
+  const reducerKeys = Object.keys(reducers);
+
+  return function(state={}, action) {
+    const nextState = {};
+
+    for(let i = 0; i < reducerKeys.length; i++) {
+      const key = reducerKeys[i];
+      const reducer = reducers[key];
+      const previousStateForKey = state[key];
+      const nextStateForKey = reducer(previousStateForKey, action);
+      nextState[key] = nextStateForKey;
+    }
+    return nextState;
+  }
+}
 
 const reducer = combineReducers({
   products: productsReducer,
