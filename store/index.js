f253658dad127ca33cc6edea3b6723303a910d84
kanishk tiwari <kanishk.swe@gmail.com>
2025-01-30 12:53:03 +0530

    middlewares in redux

--- store/index.js
+++ store/index.js
@@ -2,11 +2,13 @@ import cartSlice from "./slices/cartSlice";
 import wishlistSlice from "./slices/wishlistReducer";
 import productsSlice from "./slices/productsSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import { logger } from "./middlewares/logger";
 
-export const store = configureStore({reducer: 
-  {
+export const store = configureStore({
+  reducer: {
     products: productsSlice,
     cartItems: cartSlice,
     wishlist: wishlistSlice,
-  }
+  },
+  middleware: () => [logger]
 });
---
+++ store/middlewares/logger.js
@@ -0,0 +1,6 @@
+export const logger = store => next => action => {
+    console.log(store)
+    console.log(next)
+    console.log(action)
+    next(action)
+}
